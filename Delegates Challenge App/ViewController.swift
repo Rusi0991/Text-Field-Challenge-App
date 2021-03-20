2f1eb3c7570964ea5ceccd773279a2f8d3d93e97
Ruslan Ismayilov <ruslanismayilov484@gmail.com>
2021-03-19 20:58:05 -0700

    ZipCodeTextFieldDelegate swift file created

---
+++ Delegates Challenge App/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Delegates Challenge App
+//
+//  Created by Ruslan Ismayilov on 3/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var zipTextField: UITextField!
+    @IBOutlet weak var dollarTextField: UITextField!
+    @IBOutlet weak var lockableTextField: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    func setUpTextField(){
+        
+    }
+    
+    
+    
+//    func dollarField(amount : Double) -> String {
+//        let formatter = NumberFormatter()
+//        formatter.numberStyle = .currency
+//
+//    }
+
+}
+
---
+++ Delegates Challenge App/ZipCodeTextFieldDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  ZipCodeTextFieldDelegate.swift
+//  Delegates Challenge App
+//
+//  Created by Ruslan Ismayilov on 3/19/21.
+//
+
+import Foundation
+import UIKit
+
+class ZipCodeTextFieldCharacteristic : NSObject, UITextFieldDelegate {
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        var newtext = textField.text! as NSString
+        newtext.replacingCharacters(in: range, with: string)
+        return newtext.length <= 5
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+}
+
